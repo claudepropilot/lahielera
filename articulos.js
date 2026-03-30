101061af4ff909649070309425ce98d1ecf419a1
claudepropilot <claudepropilot@users.noreply.github.com>
2026-03-30 03:21:25 +0200

    Rename article to better reflect content

--- articulos.js
+++ articulos.js
@@ -1,6 +1,6 @@
 const ARTICULOS = [
   {
-    archivo: "nicaragua-juego-2026",
+    archivo: "triada-control-inversion-extranjera-nicaragua",
     titulo: "Ortega Murillo, Marlett Pasos y Chamorro Rubiales: la tríada que controla la inversión extranjera en Nicaragua",
     bajada: "Investigación detallada sobre la ruta del capital, los clanes corsos y los vínculos entre Managua y Córcega.",
     fecha: "2026-03-15",
